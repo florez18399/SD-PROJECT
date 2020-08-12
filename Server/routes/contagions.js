0c06c32e463560e0a0cff57bb9a487de8d93174f
florez18399 <andresfflorez1999@hotmail.com>
2020-08-11 20:52:24 -0500

    Subida de imagen a clouddinary y almacenamiento de URL en mongo

--- Server/routes/contagions.js
+++ Server/routes/contagions.js
@@ -5,6 +5,7 @@ const path = require('path')
 const insertInDb = require('../db/dbConnection').insertDocuments;
 const findOfDb = require('../db/dbConnection').findDocuments;
 const COLLECTION_CONTAGIONS = 'contagions';
+const fs = require('fs-extra')
 
 const cloudinary = require('cloudinary');
 //-----------------------Multer config ------------------------------
@@ -34,25 +35,28 @@ router.get('/', (req, res) => {
 })
 
 
-router.post('/add', upload, async(req, res) => {
+router.post('/add', upload, (req, res) => {
     let contagion = req.body;
+    let pathImage = req.file.path;
     console.log(contagion);
-    contagion.server_location = req.headers.host; 
-    contagion.path_image = '/public/images/' + req.file.filename;
-
-    console.log(contagion.path_image);
-    const resultImage = await cloudinary.v2.uploader.upload(contagion.path_image);
-
-    console.log('resut:' + resultImage);
-    console.log(contagion);
-    //res.send('Envio de imagenes');
-    insertInDb(COLLECTION_CONTAGIONS, contagion, (err, results) => {
-        if(err) {
-            res.status(500).json({message: 'Error al INSERTAR datos de contagio'})
-        }else {
-            res.status(200).json(results);
+    cloudinary.v2.uploader.upload(pathImage, (err, result) => {
+        if(err) { 
+            console.log('NO se pudo guarda en clouddinary ni en db', err);
+        }else{ 
+            contagion.public_id = result.public_id;
+            contagion.imageURL = result.url;
+            console.log('Guardando en bd con id: ', contagion.public_id)
+            insertInDb(COLLECTION_CONTAGIONS, contagion, (err, results) => {
+                if(err) {
+                    res.status(500).json({message: 'Error al INSERTAR datos de contagio'})
+                }else {
+                    res.status(200).json(results);
+                    fs.unlink(pathImage);
+                }
+            })
         }
-    })
+    });
+    //res.send('Envio de imagenes');    
 })
 
 
