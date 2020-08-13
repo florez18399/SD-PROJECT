0f3a1bb88a6ee69c4d85e28a2738f00a10f5031f
florez18399 <andresfflorez1999@hotmail.com>
2020-08-13 10:33:43 -0500

    Comandos para creacion de contenedores de monitoreo

--- Server/app.js
+++ Server/app.js
@@ -20,12 +20,17 @@ app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
 
-//app.use(cors())
+//app.use(cors(
 app.use('/contagions', routeContagions)
 app.use('/reports', routeReports)
 
 /**
  * docker network create --driver=bridge --subnet=172.20.0.0/16 red-project
  * 
- * docker run --name nginx-server -p 4010:80 --network=red-project --ip 172.20.0.2 -d  nginx
+ * docker run --name mynginx -p 4010:80 --network=red-project --ip 172.20.0.2 -d  nginx-server
+ * 
+ * Cadvisor
+ * sudo docker run -d --volume=/:/rootfs:ro --volume=/var/run:/var/run:ro --volume=/sys:/sys:ro --volume=/var/lib/docker/:/var/lib/docker:ro --volume=/dev/disk/:/dev/disk:ro --publish=9000:8080 --name=cadvisor google/cadvisor:latest
+ * Prometheus
+ * docker run -d --name prom -p 9090:9090 -v "$(pwd)"/tmp/prometheus.yml:/etc/prometheus/prometheus.yml prom/prometheus:v2.14.0
  */
